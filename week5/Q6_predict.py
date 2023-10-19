63ee5668d1913efab82732ce78d1b747de2879f5
murattkiran <131201541+murattkiran@users.noreply.github.com>
2023-10-19 22:21:19 +0000

    Added Q6_predict.py

---
+++ week5/Q6_predict.py
@@ -0,0 +1,8 @@
+import requests
+
+
+url = "http://localhost:9696/predict_flask"
+
+client = {"job": "retired", "duration": 445, "poutcome": "success"}
+response=requests.post(url, json=client).json()
+print(response)
