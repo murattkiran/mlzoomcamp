c909a1bdc8e3cd2786e1e01885a2e18328831eb1
murattkiran <131201541+murattkiran@users.noreply.github.com>
2023-12-04 16:19:09 +0000

    Updated q6_test.py for load testing

---
+++ week10/hw/q6_test.py
@@ -0,0 +1,10 @@
+from time import sleep
+import requests
+
+url = "http://localhost:9696/predict"
+client = {"job": "retired", "duration": 445, "poutcome": "success"}
+
+while True:
+    sleep(0.1)
+    response = requests.post(url, json=client).json()
+    print(response)
