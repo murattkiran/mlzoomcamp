5b603cb8076b7c48c4b48f5c775d4458423631cf
murattkiran <131201541+murattkiran@users.noreply.github.com>
2023-12-04 16:30:09 +0000

    Updated q6_test.py for testing autoscaler.

--- week10/hw/q6_test.py
+++ week10/hw/q6_test.py
@@ -4,7 +4,8 @@ import requests
 url = "http://localhost:9696/predict"
 client = {"job": "retired", "duration": 445, "poutcome": "success"}
 
-while True:
-    sleep(0.1)
+for i in range(10):  
+    sleep(1)
     response = requests.post(url, json=client).json()
     print(response)
+
